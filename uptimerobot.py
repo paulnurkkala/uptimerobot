7e95549592e5024332d1456362d7021e1389a85c
Paul Nurkkala <paul.nurkkala@thinkfirstmarketing.com>
2014-08-07 13:21:35 -0500

    adding the initial work on the uptime robot

---
+++ uptimerobot.py
@@ -0,0 +1,7 @@
+from python_uptimerobot.uptimerobot import UptimeRobot
+import ipdb
+
+UPTIME_ROBOT_API_KEY = 'u104722-e5f6960975e9dfe1a35ed7b6o'
+
+up = UptimeRobot(UPTIME_ROBOT_API_KEY)
+ipdb.set_trace()
